6ea2f14798e2a2290889e04e6418e59347917447
rferrei3 <rubens.ferreira@alticeusa.com>
2024-01-06 15:33:19 -0300

    Included first version to collect informations

---
+++ magic_formula.py
@@ -0,0 +1,152 @@
+import status_invest
+import asyncio
+from pprint import pprint
+import math
+import pandas
+import numpy as np
+import time
+
+use_cache = False
+
+async def calculate_earning_yield(ticker_info):
+    """ """
+    try:
+        ev_ebit = ticker_info.get('ev_ebit', 0)
+        ev = ticker_info.get('Valor de firma', 0)
+        print(ticker_info.get('ticker'), ev, ev_ebit)
+        ebit = ev / ev_ebit
+        earning_yield = round(ebit / ev, 2)
+    except ZeroDivisionError:
+        return 0
+
+    return earning_yield
+
+
+async def calculate_graham_vi(
+        vpa: float,
+        lpa: float,
+        max_p_l: float = 15,
+        max_p_vp: float = 1.5) -> float:
+    """Calculates the Graham VI.
+
+    :return: Graham VI
+    :rtype: float
+    """
+    if vpa <= 0 or lpa <= 0:
+        return 0
+
+    pre_vi = (max_p_l * max_p_vp) * vpa * lpa
+
+    try:
+        graham_vi = math.sqrt(pre_vi)
+    except ValueError:
+        graham_vi = 0
+
+    return round(graham_vi, 2)
+
+
+async def calculate_graham_upside(
+        current_price: float,
+        graham_vi: float) -> float:
+    """Calculates the Graham upside based on the calculated VI
+
+    :return: Graham upside
+    :rtype: float
+    """
+    if current_price <= 0 or graham_vi <= 0:
+        return 0
+
+    pre_vi = (graham_vi - current_price) / current_price
+
+    return round(pre_vi, 2)
+
+
+async def process_ticker_info(ticker_general):
+    symbol = ticker_general.get('ticker', 'Not found')
+
+    print(f'Processing ticker {symbol}')
+    page = await status_invest.get_stocks_page_info(symbol)
+    results = await status_invest.format_stock_page(page[1].content)
+    ticker_general.update(results)
+
+    roic = ticker_general.get('roic', 0)
+    vpa = ticker_general.get('vpa', 0)
+    lpa = ticker_general.get('lpa', 0)
+    p_l = ticker_general.get('p_l', 0)
+    p_vp = ticker_general.get('p_vp', 0)
+    dividend_yield = ticker_general.get('dy', 0)
+    current_price = ticker_general.get('price', 0)
+    ey = await calculate_earning_yield(ticker_general)
+    graham_vi = await calculate_graham_vi(vpa, lpa)
+    graham_upside = await calculate_graham_upside(current_price, graham_vi)
+
+    ticker_info = [
+        symbol,
+        roic,
+        vpa,
+        lpa,
+        p_l,
+        p_vp,
+        dividend_yield,
+        current_price,
+        ey,
+        graham_vi,
+        graham_upside
+    ]
+    return ticker_info
+
+
+async def main():
+    start = time.perf_counter()
+    resp = await status_invest.get_stocks_info()
+    # for item in resp.json():
+        # results = await asyncio.gather(
+            # get_stocks_historical_info(item.get('ticker')),
+            # get_stocks_historical_price(item.get('ticker')),
+            # get_stocks_page_info(item.get('ticker'))
+        # )
+        # break
+    stocks_data = []
+    tasks = []
+    roic_ignore = True
+    parallel_number = 6
+    for index, ticker in enumerate(resp.json()):
+        # if index == 10:
+            # break
+        # if ticker.get('ticker') == 'VALE3':
+        # task = await process_ticker_info(ticker)
+        tasks.append(process_ticker_info(ticker))
+        if len(tasks) > parallel_number and not use_cache:
+            stocks_data += await asyncio.gather(*tasks)
+            tasks = []
+            time.sleep(0.1)
+
+    if tasks:
+        stocks_data += await asyncio.gather(*tasks)
+    tickers_df = pandas.DataFrame(
+        columns=['symbol', 'roic', 'vpa', 'lpa', 'p_l', 'p_vp', 'dividend_yield', 'current_price', 'earning_yield', 'graham_vi', 'graham_upside'],
+        data=stocks_data
+    )
+    tickers_df.sort_values('roic', ascending=False)
+    tickers_df['roic_index_number'] = np.arange(tickers_df['roic'].count())
+    if roic_ignore:
+        tickers_df['roic_index_number'] = [0] * tickers_df['roic'].count()
+    tickers_df = tickers_df.sort_values('earning_yield', ascending=False)
+
+    tickers_df['earning_yield_index'] = np.arange(tickers_df['earning_yield'].count())
+    tickers_df['magic_index'] = tickers_df['earning_yield_index'] + tickers_df['roic_index_number']
+
+    tickers_df = tickers_df.sort_values('magic_index', ascending=True)
+    # tickers_df: pandas.DataFrame = tickers_df.loc[tickers_df['earning_yield'] > 0]
+    # data_frame.loc[0] =ticker_info 
+    tickers_df.to_excel(
+        'teste.xlsx',
+        sheet_name='stocks', index=False, engine='openpyxl',
+        freeze_panes=(1, 0)
+    )
+    print(f'Finished in {time.perf_counter() - start} seconds')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
--- status_invest.py
+++ status_invest.py
@@ -4,8 +4,23 @@ from bs4 import BeautifulSoup
 import re
 import lxml
 from pprint import pprint
+import os
+import pickle
+import datetime
+import aiofiles
+import io
+import time
+
+
+file_ttl = datetime.timedelta(minutes=50)
+use_cache = False
+
 
 async def get_stocks_info():
+    resp = await get_cached_info('get_stocks_info')
+    if resp:
+        return resp
+
     url  = 'https://statusinvest.com.br/category/advancedsearchresult?search=%7B%22Sector%22%3A%22%22%2C%22SubSector%22%3A%22%22%2C%22Segment%22%3A%22%22%2C%22my_range%22%3A%220%3B25%22%2C%22dy%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_L%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_VP%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_Ativo%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22margemBruta%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22margemEbit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22margemLiquida%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_Ebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22eV_Ebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22dividaLiquidaEbit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22dividaliquidaPatrimonioLiquido%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_SR%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_CapitalGiro%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_AtivoCirculante%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22roe%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22roic%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22roa%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22liquidezCorrente%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22pl_Ativo%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22passivo_Ativo%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22giroAtivos%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22receitas_Cagr5%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22lucros_Cagr5%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22liquidezMediaDiaria%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%7D&CategoryType=1'
     headers = {
       'accept': '*/*',
@@ -22,10 +37,16 @@ async def get_stocks_info():
     }
     async with httpx.AsyncClient(verify=False) as client:
         resp = await client.get(url, headers=headers)
+
+    await save_cached_info('get_stocks_info', resp)
     return resp
 
 
 async def get_stocks_historical_info(ticker: str):
+    resp = await get_cached_info(f'get_stocks_historical_info_{ticker}')
+    if resp:
+        return ticker, resp
+
     url = 'https://statusinvest.com.br/acao/indicatorhistorical'
     headers = {
         'accept': '*/*',
@@ -43,10 +64,15 @@ async def get_stocks_historical_info(ticker: str):
     args = f'ticker={ticker}&time=5'
     async with httpx.AsyncClient(verify=False) as client:
         resp = await client.post(f"{url}?{args}", headers=headers)
+    await save_cached_info(f'get_stocks_historical_info_{ticker}', resp)
     return ticker, resp
 
 
 async def get_stocks_historical_price(ticker: str):
+    resp = await get_cached_info(f'get_stocks_historical_price_{ticker}')
+    if resp:
+        return ticker, resp
+
     url = 'https://statusinvest.com.br/acao/tickerprice'
     headers = {
       'accept': '*/*',
@@ -58,10 +84,15 @@ async def get_stocks_historical_price(ticker: str):
     args = f'ticker={ticker}&type=4&currences%5B%5D=1'
     async with httpx.AsyncClient(verify=False) as client:
         resp = await client.post(f"{url}?{args}", headers=headers)
+
+    await save_cached_info(f'get_stocks_historical_price_{ticker}', resp)
     return ticker, resp
 
 
 async def get_stocks_page_info(ticker):
+    resp = await get_cached_info(f'get_stocks_page_info_{ticker}')
+    if resp:
+        return ticker, resp
     url = f'https://statusinvest.com.br/acoes/{ticker}'
     headers = {
       'accept': '*/*',
@@ -73,32 +104,67 @@ async def get_stocks_page_info(ticker):
     async with httpx.AsyncClient(verify=False) as client:
         resp = await client.get(url, headers=headers)
 
+    await save_cached_info(f'get_stocks_page_info_{ticker}', resp)
     return ticker, resp
 
 
-def format_stock_page(resp_page):
-    regex = re.compile('^[^\d\,%]*$')
-    page = BeautifulSoup(resp_page[1].content, "html.parser") 
+async def save_cached_info(identifier, obj):
+    if not use_cache:
+        return
+
+    if not os.path.exists('cache'):
+        os.makedirs('cache')
+
+    async with aiofiles.open(f"cache/{identifier}.cache", 'wb') as f:
+        pickled_foo = pickle.dumps(obj)
+        await f.write(pickled_foo)
+
+
+async def get_cached_info(identifier):
+    if not use_cache:
+        return None
+
+    if not os.path.exists('cache'):
+        os.makedirs('cache')
+
+    file_name = f"cache/{identifier}.cache"
+    if not os.path.exists(file_name):
+        return None
+
+    if (datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getctime(file_name))).min > file_ttl:
+        os.remove(file_name)
+        return None
+
+    async with aiofiles.open(file_name, 'rb') as f:
+        pickled_foo  = await f.read()
+
+    return pickle.loads(pickled_foo)
+
+
+async def format_stock_page(page_content):
+    regex = re.compile("^[-\d\,%\.]*$")
+    page = BeautifulSoup(page_content, "html.parser") 
     dom = lxml.etree.HTML(str(page)) 
-    elementos = dom.xpath('//div[@id="company-section"]//div[@class="info"]')
+    elements = dom.xpath('//div[@id="company-section"]//div[@class="info"]')
     result = {}
     tag = ''
-    for elemento in elementos:
-        children = elemento.xpath('./div//div//strong|./div//div//h3|./div//div//span[@class="d-inline-block mr-2"]')
+    for element in elements:
+        children = element.xpath('./div//div//strong|./div//div//h3|./div//div//span[@class="d-inline-block mr-2"]')
         for child in children:
             if child.text and not child.attrib['class'] == 'title m-0 legend-tooltip':
                 if child.tag in {'h3', 'span'}:
                     tag = child.text
                 else:
                     result[tag] = child.text
-                    if not regex.match(child.text):
-                        # print(child.text)
-                        result[tag] = float(child.text.replace('.', '').replace('%', '').replace(',', '.'))
+                    if child.text == '--%':
+                        result[tag] = '0%'
 
-                    
-    elementos_setor = dom.xpath('//div[@id="company-section"]//div[@class="top-info top-info-1 top-info-sm-2 top-info-md-n sm d-flex justify-between"]//div[contains(@class, "info")]')
-    for elemento in elementos_setor:
-        children = elemento.xpath('./div//div//strong|./div//div//span')
+                    if regex.match(result[tag]):
+                        result[tag] = float(result[tag].replace('.', '').replace('%', '').replace(',', '.'))
+
+    elements_sector = dom.xpath('//div[@id="company-section"]//div[@class="top-info top-info-1 top-info-sm-2 top-info-md-n sm d-flex justify-between"]//div[contains(@class, "info")]')
+    for element in elements_sector:
+        children = element.xpath('./div//div//strong|./div//div//span')
         for child in children:
             if child.text and not child.attrib['class'] == 'title m-0 legend-tooltip':
                 if child.tag in {'h3', 'span'}:
@@ -111,16 +177,20 @@ def format_stock_page(resp_page):
 async def main():
     resp = await get_stocks_info()
     # for item in resp.json():
-        # print(item.get('ticker'))
-
         # results = await asyncio.gather(
             # get_stocks_historical_info(item.get('ticker')),
             # get_stocks_historical_price(item.get('ticker')),
             # get_stocks_page_info(item.get('ticker'))
         # )
         # break
+    for ticker in resp.json():
+        if ticker.get('ticker') == 'VALE3':
+            pprint(ticker)
+            break
+    hist = await get_stocks_historical_info('VALE3')
+    pprint(hist[1].json())
     page = await get_stocks_page_info('VALE3')
-    results = format_stock_page(page)
+    results = format_stock_page(page[1].content)
     pprint(results)
 
 
