3f245679213c1bfa8c941752451459cffa889bc2
rferrei3 <rubens.ferreira@alticeusa.com>
2024-01-07 16:28:08 -0300

    Changed the path for the mapping on redis.conf

--- redis.sh
+++ redis.sh
@@ -1,3 +1,5 @@
+DIR=$pwd
+
 docker rm -f redis_mf 
-docker run --name redis_mf -p 6379:6379 -d -v ./redis.conf:/etc/redis/redis.conf:ro --restart always redis:latest
+docker run --name redis_mf -p 6379:6379 -d -v $DIR/src/config/redis.conf:/etc/redis/redis.conf:ro --restart always redis:latest
 
