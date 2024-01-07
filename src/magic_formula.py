744c8bee659719d42662d5b54861612192c4f592
rferrei3 <rubens.ferreira@alticeusa.com>
2024-01-07 16:29:05 -0300

    Small corrections on magic formula methods to use redis

--- src/magic_formula.py
+++ src/magic_formula.py
@@ -4,7 +4,8 @@ import math
 import pandas
 import numpy as np
 import time
-from config.settings import use_cache
+from config import settings, parser
+from databases import redis
 
 
 async def calculate_earning_yield(ticker_info):
@@ -111,38 +112,26 @@ async def main():
     parallel_number = 6
     for ticker in resp.json():
         tasks.append(process_ticker_info(ticker))
-        if len(tasks) > parallel_number and not use_cache:
+        if len(tasks) > parallel_number and not settings.use_cache:
             stocks_data += await asyncio.gather(*tasks)
             tasks = []
             time.sleep(0.1)
 
     if tasks:
         stocks_data += await asyncio.gather(*tasks)
-    tickers_df = pandas.DataFrame(
-        columns=['symbol', 'roic', 'vpa', 'lpa', 'p_l', 'p_vp', 'dividend_yield', 'current_price', 'earning_yield', 'graham_vi', 'graham_upside'],
-        data=stocks_data
+
+    credentials = parser.read_ini_file(settings.credentials_file_path)
+    if not credentials:
+        return
+    settings.credentials = credentials
+
+    identifier = 'magic_formula_main_data'
+    conn_info = redis.RedisConnectionInfo(
+        settings.credentials['redis']['hostname'],
+        settings.credentials['redis'].getint('port'),
+        settings.credentials['redis']['password'],
     )
-    tickers_df.sort_values('roic', ascending=False)
-    tickers_df['roic_index_number'] = np.arange(tickers_df['roic'].count())
-    if roic_ignore:
-        tickers_df['roic_index_number'] = [0] * tickers_df['roic'].count()
-    tickers_df = tickers_df.sort_values('earning_yield', ascending=False)
-
-    tickers_df['earning_yield_index'] = np.arange(tickers_df['earning_yield'].count())
-    tickers_df['magic_index'] = tickers_df['earning_yield_index'] + tickers_df['roic_index_number']
-
-    tickers_df = tickers_df.sort_values('magic_index', ascending=True)
-    # tickers_df: pandas.DataFrame = tickers_df.loc[tickers_df['earning_yield'] > 0]
-    # data_frame.loc[0] =ticker_info 
-    tickers_df.to_excel(
-        'teste.xlsx',
-        sheet_name='stocks', index=False, engine='openpyxl',
-        freeze_panes=(1, 0)
-    )
-
-    print(f'Finished in {time.perf_counter() - start} seconds')
-    tickers = tickers_df.to_dict(orient='records')
-    print(tickers)
+    await redis.set_object_on_redis_async(conn_info, identifier, stocks_data, time_to_live=None)
 
 
 if __name__ == '__main__':
