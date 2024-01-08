1c2acb9f908d2b62cd0d88a96cae6aed3f1eb197
rferrei3 <rubens.ferreira@alticeusa.com>
2024-01-07 21:37:45 -0300

    Included gunicorn on entrypoint

--- src/entrypoint_api.sh
+++ src/entrypoint_api.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/usr/local/bin/python api.py
+CWD=$(pwd)
+gunicorn --name=magic_formula --workers=3 --threads=15  --worker-class=gthread \
+    --max-requests 1000 --timeout 60 --bind 0.0.0.0:8090 "api:main()"
