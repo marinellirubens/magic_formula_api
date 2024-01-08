02a1332a18f7265401bea506435ab919e012172a
rferrei3 <rubens.ferreira@alticeusa.com>
2024-01-07 21:37:08 -0300

    Created example api (needs refining)

--- src/api.py
+++ src/api.py
@@ -1,19 +1,26 @@
 import asyncio
+import os
 import time
+import logging
+import datetime
 
+from flask import Flask, request, send_file
+from flask_cors import CORS
 import pandas
 import numpy as np
 from config import settings, parser
 from databases import redis
 
 
+app = Flask(__name__)
+CORS(app)
 
-async def main():
+
+@app.route('/', methods=['GET'])
+async def get_stocks_info():
     start = time.perf_counter()
-    credentials = parser.read_ini_file(settings.credentials_file_path)
-    if not credentials:
-        return
-    settings.credentials = credentials
+    roic_ignore = int(request.args.get('roic_ignore', 0))
+    format = request.args.get('format', 'json').lower()
 
     identifier = 'magic_formula_main_data'
     conn_info = redis.RedisConnectionInfo(
@@ -21,6 +28,7 @@ async def main():
         settings.credentials['redis'].getint('port'),
         settings.credentials['redis']['password'],
     )
+
     stocks_data = await redis.get_object_from_redis_async(conn_info, identifier)
     tickers_df = pandas.DataFrame(
         columns=['symbol', 'roic', 'vpa', 'lpa', 'p_l', 'p_vp', 'dividend_yield', 'current_price', 'earning_yield', 'graham_vi', 'graham_upside'],
@@ -28,28 +36,48 @@ async def main():
     )
     tickers_df.sort_values('roic', ascending=False)
     tickers_df['roic_index_number'] = np.arange(tickers_df['roic'].count())
-    roic_ignore = True
     if roic_ignore:
         tickers_df['roic_index_number'] = [0] * tickers_df['roic'].count()
+
     tickers_df = tickers_df.sort_values('earning_yield', ascending=False)
 
     tickers_df['earning_yield_index'] = np.arange(tickers_df['earning_yield'].count())
     tickers_df['magic_index'] = tickers_df['earning_yield_index'] + tickers_df['roic_index_number']
 
     tickers_df = tickers_df.sort_values('magic_index', ascending=True)
-    # tickers_df: pandas.DataFrame = tickers_df.loc[tickers_df['earning_yield'] > 0]
-    # data_frame.loc[0] =ticker_info 
-    tickers_df.to_excel(
-        'teste.xlsx',
-        sheet_name='stocks', index=False, engine='openpyxl',
-        freeze_panes=(1, 0)
-    )
+    if format == 'excel':
+        filename = f'magic_formula_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.xlsx'
+        tickers_df.to_excel(
+            filename,
+            sheet_name='stocks', index=False, engine='openpyxl',
+            freeze_panes=(1, 0)
+        )
+        file = send_file(filename)
+        os.remove(filename)
+        return file
 
-    print(f'Finished in {time.perf_counter() - start} seconds')
+    app.logger.info(f'Finished in {time.perf_counter() - start} seconds')
     tickers = tickers_df.to_dict(orient='records')
-    # print(tickers)
+    return tickers
+
+
+def main():
+    credentials = parser.read_ini_file(settings.credentials_file_path)
+    if not credentials:
+        return
+    settings.credentials = credentials
+
+    app.logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+    return app
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    app.run()
 
